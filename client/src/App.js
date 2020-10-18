0f3961906798b283be405844c2b6ac990204a837
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-10-18 10:10:03 +0300

    added authentication (login page)

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,14 @@ import { Admin, Resource } from 'react-admin';
 import { UserList } from './components/Users';
 import { PostList, PostCreate, PostEdit } from './components/Posts';
 import Dashboard from './components/Dashboard';
+import authProvider from './components/auth/AuthProvider';
 
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
