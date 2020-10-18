58da33db8db9637cf3cfb0448c31cc48bb9874c8
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-10-18 10:16:57 +0300

    added responsiveness (mobile screens) to post view

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -11,21 +11,34 @@ import {
     SelectInput,
     Edit,
     Create,
-    Filter
+    Filter,
+    SimpleList
 } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
 
-export const PostList = props => (
-    <List filters={<PostFilter />} {...props}>
-        <Datagrid>
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-            <EditButton />
-        </Datagrid>
-    </List>
-);
+export const PostList = props => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List filters={<PostFilter />} {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => `${record.views} views`}
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <ReferenceField source="userId" reference="users">
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <TextField source="title" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
 
 const PostTitle = ({ record }) => {
     return <span>Post {record ? `"${record.title}"` : ''}</span>;
