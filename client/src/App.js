65e7e4e212f898ae31b5bac48341e3f3bea1068e
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-10-18 10:01:09 +0300

    added view Posts section and added Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
+import jsonServerProvider from 'ra-data-json-server';
+
 import { Admin, Resource } from 'react-admin';
 import { UserList } from './components/Users';
-import jsonServerProvider from 'ra-data-json-server';
+import { PostList, PostCreate, PostEdit } from './components/Posts';
+import Dashboard from './components/Dashboard';
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={UserList} />
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 )
 
