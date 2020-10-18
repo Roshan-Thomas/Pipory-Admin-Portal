9c32c99b5e06b234c7e3963087d71bb939bfdaf9
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-10-18 09:26:44 +0300

    getting users from api and displaying in datagrid

---
+++ client/src/components/Users.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import MyURLField from './MyURLField';
+
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <EmailField source="email" />
+            <MyURLField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+)
