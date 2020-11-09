f815c0117c174fecf3abf560d76c4e75e1faa851
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-11-09 12:48:20 +0300

    added fields to UserEdit view

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import jsonServerProvider from 'ra-data-json-server';
 
 import { Admin, Resource } from 'react-admin';
-import { UserList } from './components/Users/Users';
+import { UserList, UserEdit } from './components/Users/Users';
 import { PostList, PostCreate, PostEdit } from './components/Posts/Posts';
 import Dashboard from './components/Dashboard/Dashboard';
 import authProvider from './components/auth/AuthProvider';
@@ -14,7 +14,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
-        <Resource name="users" list={UserList} icon={UserIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} edit={UserEdit}  />
         <Resource name="influencer" list={UserList} icon={UserIcon} />
         <Resource name="ads" list={PostList} icon={PostIcon} />
     </Admin>
--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,5 +1,15 @@
 import * as React from 'react';
-import { List, Datagrid, TextField, EmailField, NumberField } from 'react-admin';
+import { 
+    List, 
+    Datagrid, 
+    TextField, 
+    EmailField, 
+    NumberField,
+    Edit, 
+    SimpleForm, 
+    TextInput, 
+    NumberInput,
+} from 'react-admin';
 
 export const UserList = props => (
     <List {...props}>
@@ -13,4 +23,59 @@ export const UserList = props => (
             <TextField source="" label="Activity" />
         </Datagrid>
     </List>
+)
+
+const UserTitle = ({ record }) => {
+    return <span>{record ? `${record.name}` : ''}</span>;
+};
+
+export const UserEdit = props => (
+    <Edit title={<UserTitle />} {...props}>
+        <SimpleForm>
+
+            {/* User Data */}
+            <TextField label="User" />
+            <TextInput disabled label="ID" source="id" />
+            <TextInput label="Username" source="company.name" />
+            <TextInput label="Email" source="email" />
+            <TextInput disabled label="Followers" source="address.zipcode" />
+            <TextInput disabled label="Following" source="address.zipcode" />
+            <NumberInput disabled label="# of Posts" source="id" />
+            <TextInput disabled label="Activity" />
+            
+            {/* User Interests  */}
+            <TextField label="Interests" />
+            <TextInput label="Interests" source="company.catchPhrase" fullWidth />
+
+            {/* User's Post Details */}
+            <TextField label="Posts" />
+            <NumberInput label="Impressions" source="address.geo.lng" />
+            <NumberInput label="Coins" source="address.geo.lng" />
+            <NumberInput label="Likes" source="address.geo.lng" />
+            <NumberInput label="Comments" source="address.geo.lng" />
+            <NumberInput label="Saves" source="address.geo.lng" />
+            <NumberInput label="Shares" source="address.geo.lng" />
+
+            {/* Users's Referrals */}
+            <TextField label="Referrals" />
+            <TextInput label="User Referral Code" source="phone" />
+            <NumberInput label="New Users by using the Code" source="address.geo.lng" />
+
+            {/* User's Coins Details */}
+            <TextField label="Coins" />
+            <NumberInput label="Total Coins" source="address.geo.lng" />
+            <NumberInput label="Total Coins from daily rewards" source="address.geo.lng" />
+            <NumberInput label="Total Coins from referrals" source="address.geo.lng" />
+            <NumberInput label="Total Coins from posts" source="address.geo.lng" />
+            <NumberInput label="Total Coins from missions" source="address.geo.lng" />
+
+            {/* User's Offers */}
+            <TextField label="Offers" />
+            <NumberInput label="Purchased Offers" source="address.geo.lat" />
+            <NumberInput label="Rated Offers" source="address.geo.lat" />
+            <NumberInput label="Liked Offers" source="address.geo.lat" />
+            <NumberInput label="Shared Offers" source="address.geo.lat" />
+
+        </SimpleForm>
+    </Edit>
 )
